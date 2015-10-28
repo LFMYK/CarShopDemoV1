710018d666e501c0ce95283c915601cd611bc1de
YKLFM <421619670@qq.com>
2015-10-28 10:32:09 +0800

    no message

--- client/js/car_list.js
+++ client/js/car_list.js
@@ -1,21 +1,28 @@
+'use strict';
+
 $(function() {
   $('#delConfirm').on('show.bs.modal', function(event) {
     var button = $(event.relatedTarget);
     var id = button.attr('data-id');
     var okbtn = $('#delConfirmbtnOk');
     okbtn.attr('data-id', id);
+
+
+    console.log('上面输出的ID为：');
+    console.log(id);
   });
 
   $('#delConfirmbtnOk').click(function(event) {
     var target = $(event.target);
-    var id = target.data('id');
+    var id = target.attr('data-id');
     var tr = $('.item-id-' + id);
 
+    console.log('输出的ID为：');
     console.log(id);
 
     $.ajax({
       type: 'DELETE',
-      url: '/admin/list?id=' + id
+      url: '/admin/car/list?id=' + id
     }).done(function(results) {
       console.log(results);
       if (results.ok === 1) {
--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ var app = express();
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/carShop');
 
-var ModelCar = require('./models/car');
+
 
 var path = require('path');
 
@@ -26,144 +26,30 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-app.get('/', function(req, res, next) {
-  ModelCar.fetch(function(err, cars) {
-    if (err) {
-      return next(err);
-    }
-    res.render('index', {
-      title: '汽车商城 首页',
-      cars: cars
-    });
-  });
-});
 
+require('./routes')(app);
 
-app.get('/admin/car/list', function(req, res, next) {
-  ModelCar.fetch(function(err, cars) {
-    if (err) {
-      return next(err);
-    }
-    res.render('car_list.jade', {
-      title: '汽车商城 列表页',
-      cars: cars
-    });
-  });
-});
-
-app.get('/car/:id', function(req, res, next) {
-  var id = req.params.id;
-  ModelCar.findById(id, function(err, car) {
-    if (err) {
-      return next(err);
-    }
-    res.render('car_detail', {
-      title: '汽车商城 详情页',
-      car: car
-    });
-  });
-});
-
-
-
-app.get('/admin/car/new', function(req, res) {
-  res.render('car_admin', {
-    title: '汽车商城 后台录入页',
-    car: {}
-  });
-});
-
-app.get('/admin/car/update/:id', function(req, res, next) {
-  var id = req.params.id;
-  ModelCar.findById(id, function(err, car) {
-    if (err) {
-      return next(err);
-    }
-    res.render('car_admin', {
-      title: '汽车商城 后台录入页',
-      car: car
-    });
-  });
-});
-
-
-app.post('/admin/car', function(req, res, next) {
-  var carObj = req.body.car;
-  if (!carObj) {
-    return res.status(400).send('找不到合法数据.');
-  }
-  var id = carObj._id;
-  if (!id) {
-    //新增
-    var docCar = new ModelCar(carObj);
-    docCar.save(function(err, _car) {
-      if (err) {
-        return next(err);
-      }
-      return res.redirect('/car/' + _car._id);
-    });
-  } else {
-    //修改
-    /*
-    ModelCar.findByIdAndUpdate(id, carObj, function(err, _car) {
-      if (err) {
-        return next(err);
-      }
-      return res.redirect('/car/' + id);
-    });
-    */
-    //修改
-    ModelCar.findOneAndUpdate({
-        _id: id
-      }, carObj)
-      .exec(function(err, _car) {
-        if (err) {
-          return next(err);
-        }
-        return res.redirect('/car/' + _car._id);
-      });
-  }
-});
-
-app.post('/admin/car',function(req,res){
-  console.log(req.body);
-
-  res.sendStatus(200);
-});
-
-
-// /admin/list?id=xxxxx
-
-app.delete('/admin/list', function(req, res, next) {
-  var id = req.query.id;
-  if (id) {
-    ModelCar.findByIdAndRemove(id, function(err, _car) {
-      if (err) {
-        res.status(500).json({
-          ok: 0
-        });
-        return next(err);
-      } else {
-        res.json({
-          ok: 1
-        });
-      }
-    });
-  } else {
-    res.json({
-      ok: 0
-    });
-  }
-});
 
 app.listen(port);
 
 console.log('汽车商城网站服务已启动,监听端口号:'+port);
 
-var errorhandler = require('errorhandler');
-app.use(errorhandler);
 
-require('express-debug')(app, {
-  depth: 10,
-  panels: ['locals', 'request', 'session', 'template', 'software_info', 'nav']
-});
+mongoose.set('debug',true);
+
+app.locals.pretty=true;
+
+if (process.env.NODE_ENV === 'development') {
+
+  mongoose.set('debug', true);
+
+  require('express-debug')(app, {
+    depth: 4,
+    panels: ['locals', 'request', 'session', 'template', 'software_info', 'nav']
+  });
+
+  var errorhandler = require('errorhandler');
+  app.use(errorhandler);
+
+}
+
